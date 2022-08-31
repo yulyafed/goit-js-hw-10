4c4f322ce90208924bfc80e7081fc69afceb67d2
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-31 23:43:40 +0300

    add changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,7 @@
-fetchCountries(name)
+function fetchCountries(name){ 
+    const url = 'https://restcountries.com/v2/all?fields=name,capital,population,flag,languages';
+
+    fetch(url)
+        .then(response => { return response.json(); })
+        .then(country => { console.log(country) });ee
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './css/styles.css';
 
 const inputCountry = document.querySelector('[id="search-box"]');
-// inputCountry.addEventListener('click');
+e
 const DEBOUNCE_DELAY = 300;
-const url = 'https://restcountries.com/v2/all?fields=name,capital,population,lang';
 
-fetch(url)
-    .then(response => { return response.json();})
-    .then(country => { console.log(country) });
+eeeee
