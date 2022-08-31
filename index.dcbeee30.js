78b689a5ec029a3f2d518f40ff5086316c8709fa
yulyafed <99392460+yulyafed@users.noreply.github.com>
2022-08-31 16:25:08 +0000

    Deploying to gh-pages from @ yulyafed/goit-js-hw-10@c15210a201057e70c85108887df718fff4ed9606 🚀

---
+++ index.dcbeee30.js
@@ -0,0 +1,2 @@
+document.querySelector('[id="search-box"]');fetch("https://restcountries.com/v2/all?fields=name,capital,population,lang").then((e=>e.json())).then((e=>{console.log(e)}));
+//# sourceMappingURL=index.dcbeee30.js.map
---
+++ index.ec05519c.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector('[id="search-box"]');fetch("https://restcountries.com/v2/all?fields=name,capital,population,lang").then((function(n){return n.json()})).then((function(n){console.log(n)}))}();
+//# sourceMappingURL=index.ec05519c.js.map
