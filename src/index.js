b9f3760753201787294753d4b8e7d05c9c977aaa
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-09-01 13:46:09 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const DEBOUNCE_DELAY = 300;
 
 function onInputCountryClick(e) { 
     
-    // const inputCountryValue = e.target.value;
-    // console.log(inputCountryValue)
+    const inputCountryValue = e.target.value;
+    console.log(inputCountryValue)
     // const url = `https://restcountries.com/v2/all?fields=name,capital,population,flag,languages`;
-    const url = 'https://restcountries.com/v2/name';
+    const url = `https://restcountries.com/v2/name/${inputCountryValue}?fields=name,capital,population,flag,languages`;
        fetch(url)
         .then(response => { return response.json(); })
         .then(country => { console.log(country) });
