cc487a48dcd7c0e60633f93ba994ffc20452872f
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-09-04 12:47:42 +0300

    add changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,73 +1,73 @@
 import Notiflix from 'notiflix';
-export { fetchCountries, countryList, countryBox};
-    
+export { fetchCountries, countryList, countryBox };
+
 const countryList = document.querySelector('.country-list');
 const countryBox = document.querySelector('.country-info');
 
-    function fetchCountries(inputCountryValue) { 
-   
+function fetchCountries(inputCountryValue) {
+
     const url = `https://restcountries.com/v2/name/${inputCountryValue}?fields=name,capital,population,flag,languages`;
-        
-        fetch(url)
-            .then(response => {
 
-                if (!response.ok) {
-                    Notiflix.Notify.failure('Oops, there is no country with that name');
-                }
+    fetch(url)
+        .then(response => {
 
-                return response.json();
-            })
-            .then(countries => {
-                console.log(countries)
-                
-                if (countries.length > 10) {
-                    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-                };
-                if (countries.length >= 2 && countries.length <= 10) {
-                    function renderCountryList(countries) {
-                        const markup = countries
-                            .map((country) => {
-                                return `<li class = "item">
+            if (!response.ok) {
+                Notiflix.Notify.failure('Oops, there is no country with that name');
+            }
+
+            return response.json();
+        })
+        .then(countries => {
+            console.log(countries)
+
+            if (countries.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            };
+            if (countries.length >= 2 && countries.length <= 10) {
+                function renderCountryList(countries) {
+                    const markup = countries
+                        .map((country) => {
+                            return `<li class = "item">
                             <img src ='${country.flag}'>
                             <p class = "text">${country.name}</p>
                             </li>`;
-                            })
-                            .join("");
-                        countryList.innerHTML = markup;
-                    };
-
-                    renderCountryList(countries);
-                    
+                        })
+                        .join("");
+                    countryList.innerHTML = markup;
                 };
-                if (countries.length === 1) {
-                    function renderCountryList(countries) {
-                        const markup = countries
-                            .map((country) => {
-                                return `<li class = "item">
+
+                renderCountryList(countries);
+
+            };
+            if (countries.length === 1) {
+                function renderCountryList(countries) {
+                    const markup = countries
+                        .map((country) => {
+                            return `<li class = "item">
                                 <img src ='${country.flag}'>
                                 <p class = "text-box">${country.name}</p>
                                 </li>`;
-                            })
-                            .join("");
-                        countryList.innerHTML = markup;
-                    }
+                        })
+                        .join("");
+                    countryList.innerHTML = markup;
+                }
 
-                    renderCountryList(countries);
+                renderCountryList(countries);
 
-                    function renderCountryBox(countries) {
-                        const markup = countries
-                            .map((country) => {
-                                return `<li>
+                function renderCountryBox(countries) {
+                    const markup = countries
+                        .map((country) => {
+                            return `<li>
                                 <p><span>Capital</span>: ${country.capital}</p>
                                 <p><span>Population</span>: ${country.population}</p>
-                                <p><span>Languages</span>: ${country.languages[0].name}</p>
+                                <p><span>Languages</span>: ${country.languages.map((language) => language.name).join(", ")}</p>
                                 </li>`;
-                            })
-                            .join("");
-                        countryBox.innerHTML = markup;
-                    }
+                        })
+                        .join("");
+                    countryBox.innerHTML = markup;
+                }
 
-                    renderCountryBox(countries);
-                };
-            }).catch(error => { console.log(error) });
-        }
+                renderCountryBox(countries);
+            };
+        }).catch(error => { console.log(error) });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from "lodash.debounce";
-import { fetchCountries, countryList, countryBox} from "./fetchCountries";
+import { fetchCountries, countryList, countryBox } from "./fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,18 +8,13 @@ const inputCountry = document.querySelector('[id="search-box"]');
 
 inputCountry.addEventListener('click', debounce(onInputCountryClick, DEBOUNCE_DELAY));
 
-function onInputCountryClick(e) { 
-    
-    const inputCountryValue = e.target.value;
-    
-    const trim = inputCountryValue.trim();
-    
-    if (!inputCountryValue || !trim) { 
+function onInputCountryClick(e) {
+    const inputCountryValue = e.target.value.trim();
+    if (!inputCountryValue) {
         countryList.innerHTML = '';
         countryBox.innerHTML = '';
         return;
     }
 
     fetchCountries(inputCountryValue);
-       
 }
